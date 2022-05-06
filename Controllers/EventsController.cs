cdeaa80bdc696bf062d37aeecc3a8da25ac2c343
Joshua Lynch <joshua.lynch@hotmail.com>
2022-05-06 14:56:39 -0400

    adding code before styling

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        public static string newFood = "";
+        public static List<string> foods = new List<string>();
+
+        public IActionResult Index()
+        {
+            ViewBag.Foods = foods;
+            if(newFood != "")
+            {
+                ViewBag.Foods.Add(newFood);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult FormDisplay()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/FormDisplay")]
+        public IActionResult FormPost(string inputName = "Avocado Toast")
+        {
+            newFood = inputName;
+            return Redirect("/Events");
+        }
+    }
+}
